51107f4fe82c8903be02014e396c14fea8a54bac
kalangoti <kalangoti@gmail.com>
2022-04-15 19:26:50 -0300

    updated the spring framework version

--- domain/src/main/java/module-info.java
+++ domain/src/main/java/module-info.java
@@ -1,6 +1,6 @@
 
 // Responsável por definir as regras de visibilidade do modulo sistema.
-module conta.sistema {
+module domain {
     // usa spring
     requires javax.inject;
     requires spring.tx;
